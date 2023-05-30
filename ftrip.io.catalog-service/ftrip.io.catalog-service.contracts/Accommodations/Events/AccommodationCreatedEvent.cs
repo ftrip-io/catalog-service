743b79d3f253f7341a63adc86ee1ec5507dd43d3
Panic <panic.milos99@gmail.com>
2023-05-30 22:35:29 +0200

    feat: added accomodation created event.

---
+++ ftrip.io.catalog-service/ftrip.io.catalog-service.contracts/Accommodations/Events/AccommodationCreatedEvent.cs
@@ -0,0 +1,16 @@
+﻿using ftrip.io.framework.Domain;
+using System;
+
+namespace ftrip.io.catalog_service.contracts.Accommodations.Events
+{
+    public class AccommodationCreatedEvent : Event<string>
+    {
+        public Guid AccommodationId { get; set; }
+        public Guid HostId { get; set; }
+
+        public AccommodationCreatedEvent()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+}
