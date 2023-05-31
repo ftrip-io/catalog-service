6abfceeeb3b5dc07c2845e16ee7cac2bcf8e1412
Panic <panic.milos99@gmail.com>
2023-05-31 12:13:48 +0200

    feat: added accomodation created event. (#11)

--- ftrip.io.catalog-service/ftrip.io.catalog-service/Accommodations/UseCases/CreateAccommodation/CreateAccommodationRequestHandler.cs
+++ ftrip.io.catalog-service/ftrip.io.catalog-service/Accommodations/UseCases/CreateAccommodation/CreateAccommodationRequestHandler.cs
@@ -1,9 +1,11 @@
 ﻿using AutoMapper;
 using ftrip.io.catalog_service.Accommodations.Domain;
 using ftrip.io.catalog_service.Amenities;
+using ftrip.io.catalog_service.contracts.Accommodations.Events;
 using ftrip.io.catalog_service.PropertyTypes;
 using ftrip.io.framework.ExceptionHandling.Exceptions;
 using ftrip.io.framework.Globalization;
+using ftrip.io.framework.messaging.Publisher;
 using ftrip.io.framework.Persistence.Contracts;
 using MediatR;
 using System.Linq;
@@ -20,6 +22,7 @@ namespace ftrip.io.catalog_service.Accommodations.UseCases.CreateAccommodation
         private readonly IPropertyTypeRepository _propertyTypeRepository;
         private readonly IMapper _mapper;
         private readonly IStringManager _stringManager;
+        private readonly IMessagePublisher _messagePublisher;
 
         public CreateAccommodationRequestHandler(
             IUnitOfWork unitOfWork,
@@ -27,7 +30,8 @@ namespace ftrip.io.catalog_service.Accommodations.UseCases.CreateAccommodation
             IAmenityRepository amenityRepository,
             IPropertyTypeRepository propertyTypeRepository,
             IMapper mapper,
-            IStringManager stringManager)
+            IStringManager stringManager,
+            IMessagePublisher messagePublisher)
         {
             _unitOfWork = unitOfWork;
             _accommodationRepository = accommodationRepository;
@@ -35,6 +39,7 @@ namespace ftrip.io.catalog_service.Accommodations.UseCases.CreateAccommodation
             _propertyTypeRepository = propertyTypeRepository;
             _mapper = mapper;
             _stringManager = stringManager;
+            _messagePublisher = messagePublisher;
         }
 
         public async Task<Accommodation> Handle(CreateAccommodationRequest request, CancellationToken cancellationToken)
@@ -56,12 +61,25 @@ namespace ftrip.io.catalog_service.Accommodations.UseCases.CreateAccommodation
 
             await _unitOfWork.Commit(cancellationToken);
 
+            await PublishAccommodationCreatedEvent(createdAccommodation, cancellationToken);
+
             return createdAccommodation;
         }
 
         private async Task<Accommodation> CreateAccommodation(Accommodation accommodation, CancellationToken ct)
         {
             return await _accommodationRepository.Create(accommodation, ct);
+        }
+
+        private async Task PublishAccommodationCreatedEvent(Accommodation accommodation, CancellationToken cancellationToken)
+        {
+            var accommodationCreated = new AccommodationCreatedEvent()
+            {
+                AccommodationId = accommodation.Id,
+                HostId = accommodation.HostId
+            };
+
+            await _messagePublisher.Send<AccommodationCreatedEvent, string>(accommodationCreated, cancellationToken);
         }
     }
 }
