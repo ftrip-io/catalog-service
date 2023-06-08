f069155d60f146e804755c9f778a5d4485005546
Albert Makan <68916222+albertmakan@users.noreply.github.com>
2023-06-08 12:17:03 +0200

    feat: added read by ids endpoint (#14)

---
+++ ftrip.io.catalog-service/ftrip.io.catalog-service/Accommodations/UseCases/ReadByIds/ReadByIdsQuery.cs
@@ -0,0 +1,12 @@
+﻿using ftrip.io.catalog_service.Accommodations.Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+
+namespace ftrip.io.catalog_service.Accommodations.UseCases.ReadByIds
+{
+    public class ReadByIdsQuery : IRequest<IEnumerable<Accommodation>>
+    {
+        public Guid[] Ids { get; set; }
+    }
+}
