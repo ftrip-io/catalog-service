ca83a7ee9f2e4963ac30eb4855fe25c0ad731f7a
Albert Makan <68916222+albertmakan@users.noreply.github.com>
2023-06-23 20:45:28 +0200

    Feat/delete by host (#18)
    * feat: user deleted event consumer, delete by host id
    
    * fix: only when user type is host

---
+++ ftrip.io.catalog-service/ftrip.io.catalog-service/Accommodations/Consumers/UserDeletedEventConsumer.cs
@@ -0,0 +1,29 @@
+﻿using ftrip.io.catalog_service.Accommodations.UseCases.DeleteByHostId;
+using ftrip.io.user_service.contracts.Users.Events;
+using MassTransit;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ftrip.io.catalog_service.Accommodations.Consumers
+{
+    public class UserDeletedEventConsumer : IConsumer<UserDeletedEvent>
+    {
+        private readonly IMediator _mediator;
+
+        public UserDeletedEventConsumer(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task Consume(ConsumeContext<UserDeletedEvent> context)
+        {
+            var deletedUser = context.Message;
+
+            if (deletedUser.UserType == "Host")
+                await _mediator.Send(new DeleteByHostIdRequest { HostId = Guid.Parse(deletedUser.UserId) }, CancellationToken.None);
+        }
+
+    }
+}
