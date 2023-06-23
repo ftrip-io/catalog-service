254f49690a6029c819d66e991300290726b177e3
albertmakan <makanalbert@gmail.com>
2023-06-23 19:34:14 +0200

    feat: user deleted event consumer, delete by host id

---
+++ ftrip.io.catalog-service/ftrip.io.catalog-service/Accommodations/Consumers/UserDeletedEventConsumer.cs
@@ -0,0 +1,28 @@
+﻿using ftrip.io.catalog_service.Accommodations.UseCases.DeleteByHostId;
+using ftrip.io.user_service.contracts.Users.Events;
+using MassTransit;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ftrip.io.catalog_service.Accommodations.Consumers
+{
+    public class UserDeletedEventConsumer : IConsumer<UserDeletedEvent>
+    {
+        private readonly IMediator _mediator;
+
+        public UserDeletedEventConsumer(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task Consume(ConsumeContext<UserDeletedEvent> context)
+        {
+            var userId = context.Message.UserId;
+
+            await _mediator.Send(new DeleteByHostIdRequest { HostId = Guid.Parse(userId) }, CancellationToken.None);
+        }
+
+    }
+}
