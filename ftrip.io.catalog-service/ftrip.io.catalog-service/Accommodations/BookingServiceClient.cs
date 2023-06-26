3fbf65ede3c5eb694fd9a4ad08ccfb766c475438
Panic <panic.milos99@gmail.com>
2023-06-27 00:41:21 +0200

    fix: passing jwt and correlation id to booking service

--- ftrip.io.catalog-service/ftrip.io.catalog-service/Accommodations/BookingServiceClient.cs
+++ ftrip.io.catalog-service/ftrip.io.catalog-service/Accommodations/BookingServiceClient.cs
@@ -1,5 +1,7 @@
 ﻿using ftrip.io.catalog_service.contracts.Accommodations;
+using ftrip.io.framework.Contexts;
 using ftrip.io.framework.Correlation;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,28 +15,37 @@ namespace ftrip.io.catalog_service.Accommodations
     public interface IBookingServiceClient
     {
         Task<IEnumerable<AccommodationOccupancy>> GetOccupancies(Guid accommodationId, DateTime periodFrom, DateTime periodTo);
+
         Task<IEnumerable<Guid>> CheckAvailability(List<Guid> accommodationIds, DateTime periodFrom, DateTime periodTo, CancellationToken cancellationToken);
     }
 
     public class BookingServiceClient : IBookingServiceClient
     {
         private readonly HttpClient _httpClient;
+        private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly CorrelationContext _correlationContext;
 
         public BookingServiceClient(
             IHttpClientFactory httpClientFactory,
+            IHttpContextAccessor httpContextAccessor,
             CorrelationContext correlationContext)
         {
             _httpClient = httpClientFactory.CreateClient("booking");
+            _httpContextAccessor = httpContextAccessor;
             _correlationContext = correlationContext;
         }
 
         public async Task<IEnumerable<AccommodationOccupancy>> GetOccupancies(Guid accommodationId, DateTime periodFrom, DateTime periodTo)
         {
-            var responseStr = await _httpClient.GetStringAsync(
-                $"api/accommodation-occupancy?" +
-                $"AccommodationId={accommodationId}&PeriodFrom={periodFrom:yyyy-MM-dd}&PeriodTo={periodTo:yyyy-MM-dd}"
-            );
+            var requestUrl = $"api/accommodation-occupancy?" +
+                $"AccommodationId={accommodationId}&PeriodFrom={periodFrom:yyyy-MM-dd}&PeriodTo={periodTo:yyyy-MM-dd}";
+            using var request = new HttpRequestMessage(HttpMethod.Get, requestUrl);
+            request.Headers.Add("Authorization", _httpContextAccessor.HttpContext.Request.Headers.FirstOrDefault(a => a.Key == "Authorization").Value.ToString());
+            request.Headers.Add(CorrelationConstants.HeaderAttriute, _correlationContext.Id);
+
+            var response = await _httpClient.SendAsync(request, CancellationToken.None);
+            response.EnsureSuccessStatusCode();
+            var responseStr = await response.Content.ReadAsStringAsync();
 
             return JsonSerializer.Deserialize<IEnumerable<AccommodationOccupancy>>(responseStr, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
         }
